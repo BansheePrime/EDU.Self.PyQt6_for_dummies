e5e33200c778846ea14d6bd5de49b86c9cd3c42e
BansheePrime <Cmapuk-XpeHoB@yandex.ru>
2024-03-23 14:34:08 +0300

    adding oop version of helloworld window

---
+++ oop_hello.py
@@ -0,0 +1,22 @@
+from PyQt6 import QtWidgets, QtCore
+
+class MyWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.label = QtWidgets.QLabel("Привет, мир!")
+        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.btnQuit = QtWidgets.QPushButton("&Закрыть")
+        self.vbox = QtWidgets.QVBoxLayout()
+        self.vbox.addWidget(self.label)
+        self.vbox.addWidget(self.btnQuit)
+        self.setLayout(self.vbox)
+        self.btnQuit.clicked.connect(QtWidgets.QApplication.instance().quit)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.setWindowTitle("Другие правила")
+    window.resize(300, 70)
+    window.show()
+    sys.exit(app.exec())
